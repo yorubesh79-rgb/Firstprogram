e2df2f6484e21e66455231e673dc69a8dc08b6c2
yorubesh79-rgb <yorubesh79@gmail.com>
2025-12-05 11:20:52 +0530

    Add files via upload
    learn forloop

---
+++ Learnforloop.java
@@ -0,0 +1,15 @@
+package learnautomation1;
+
+public class Learnforloop {
+
+	public static void main(String[] args) {
+		
+		int num=5;
+		
+		for (int i=1;i<=10;i++) {
+			
+			System.out.println (num + "x" + i + "=" + (num * i));
+		}
+	}
+
+}
