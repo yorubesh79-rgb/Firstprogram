7a785c76f6e827e907697f63e9ac855d5ad2fa6c
yorubesh79-rgb <yorubesh79@gmail.com>
2025-12-09 11:54:20 +0530

    Add files via upload
    amazon ss

--- Screenshot.java
+++ Screenshot.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Screenshot {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Throwable {
 		ChromeDriver driver=  new ChromeDriver();
 		
          driver.get("https://www.amazon.in/");
